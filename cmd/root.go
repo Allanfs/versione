f5c70686e7a6ea788c178ba2b1c391d00425d012
Allan Neri Fernandes da Silva <allan.silva@servicenet.com.br>
2021-06-25 23:36:03 -0300

    feat: bump de versão parametrizado

--- cmd/root.go
+++ cmd/root.go
@@ -25,6 +25,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+var Version string = "dev"
+
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
@@ -36,23 +38,24 @@ com base na versão atual e na branch desenvolvida`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Version: Version,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.InitDefaultVersionFlag()
 	cobra.CheckErr(rootCmd.Execute())
+
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.versione.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
@@ -73,12 +76,8 @@ func initConfig() {
 		viper.SetConfigName(".versione")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
-
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	} else {
-		fmt.Fprintln(os.Stderr, err)
 	}
 }
